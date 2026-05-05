06a210a529b50c37ccdcb199c4c56e984ff76321
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2026-05-05 15:23:21 +0300

    restore layout from stash

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="">
-      <Outlet />
+    <div className=" theme flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 w-full px-[108px] mx-auto">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,17 @@ import { store } from "./app/store";
 import "./index.css";
 import App from "./App.tsx";
 
+import { HomePage } from "./pages/HomePage/HomePage.tsx";
+import { ClinicsPage } from "./pages/ClinicsPage/ClinicsPage.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="clinics" element={<ClinicsPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </Provider>,
